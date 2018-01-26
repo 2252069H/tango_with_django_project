2f71ba20461538e77268d6703672c7a9ec841601
Jonathan Henderson <2252069h@student.gla.ac.uk>
2018-01-26 15:55:51 +0000

    end of 4.3 before exercises

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -21,6 +21,12 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 #Create path to static directory
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 
+#Create path to media directory
+MEDIA_DIR = os.path.join(BASE_DIR,'media')
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -68,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
