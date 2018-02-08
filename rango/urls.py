9ba179af884c5f2f76827e9f293ff14b17729400
Jonathan Henderson <2252069h@student.gla.ac.uk>
2018-02-08 13:28:39 +0000

    End of exercise 6 before exercises

--- rango/urls.py
+++ rango/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from rango import views
 
-#Items on page 48 may need to be added
-
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
         url(r'^about/',views.about,name='about'),
+        url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+            views.show_category, name='show_category')
 ]
